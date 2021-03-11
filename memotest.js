304dda08921406005458deec06bbf51812457f76
Andrea Lecuona <andre.lecuona91@gmail.com>
2021-03-11 11:29:57 -0300

    Refactorizo código

--- memotest.js
+++ memotest.js
@@ -5,37 +5,37 @@ let firstCard;
 let secondCard;
 
 const flipCards = function() {
-    console.log(this);
     this.classList.toggle('flip');
 
     //identify selected cards
-
-    //first card
     if(!alreadyFlippedSomething){
         alreadyFlippedSomething = true;
         firstCard = this;
-
-        console.log(alreadyFlippedSomething, firstCard);
-    
-    //second card
     } else {
         alreadyFlippedSomething = false;
         secondCard = this;
 
-        console.log(alreadyFlippedSomething, secondCard);
+        checkMatch();
+    };
 
-        //check match
-        if(firstCard.dataset.icon === secondCard.dataset.icon){
-            firstCard.removeEventListener('click', flipCards);
-            secondCard.removeEventListener('click', flipCards);
-        } else {
-            setTimeout( () => {
-                firstCard.classList.remove('flip');
-                secondCard.classList.remove('flip');
-            }, 1500);
-        }
-    }
+};
 
+const checkMatch = function() {
+    let doMatch = firstCard.dataset.icon === secondCard.dataset.icon;
+    doMatch ? blockCards() : unflip();
+};
+
+
+const blockCards = function() {
+    firstCard.removeEventListener('click', flipCards);
+    secondCard.removeEventListener('click', flipCards);
+};
+
+const unflip = function() {
+    setTimeout( () => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 1500);
 };
 
 cards.forEach(card => card.addEventListener('click', flipCards));
